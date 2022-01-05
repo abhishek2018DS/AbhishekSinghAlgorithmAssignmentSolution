79a773dad1fb42e931930e430c4ed2d08785dbc9
abhishek2018DS <42770193+abhishek2018DS@users.noreply.github.com>
2022-01-05 13:57:37 +0530

    Create StockSearching.java

---
+++ src/com/greatlearning/stocksearching/StockSearching.java
@@ -0,0 +1,33 @@
+package com.greatlearning.stocksearching;
+import java.util.Arrays;
+
+public class StockSearching
+{
+    // Returns index of x if it is present in arr[l..
+    // r], else return -1
+    public int stockSearch(double arr[], int l, int r, double x)
+    {
+        if (r>=l)
+        {
+            int mid = l + (r - l)/2;
+
+            // If the element is present at the
+            // middle itself
+            if (arr[mid] == x)
+            return mid;
+
+            // If element is smaller than mid, then
+            // it can only be present in left subarray
+            if (arr[mid] > x)
+            return stockSearch(arr, l, mid-1, x);
+
+            // Else the element can only be present
+            // in right subarray
+            return stockSearch(arr, mid+1, r, x);
+        }
+
+        // We reach here when element is not present
+        // in array
+        return -1;
+    }
+}    
